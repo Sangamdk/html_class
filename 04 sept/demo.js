c8e23294c3cf26d1a9cad127fe207931d9306d4c
Kumar Sangam <40032010+Sangamdk@users.noreply.github.com>
2024-09-04 12:09:42 +0530

    Add files via upload
    javascript 1st class

---
+++ 04 sept/demo.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById("demo").innerHTML = "hiiii";
+document.getElementById("total").innerHTML=58*3;
+document.write(5 + 6);
+function mybt()
+{
+document.getElementById("demo1").innerHTML="berhampur";
+}
