27b83e955da2e0216bf027858c1f77ac4b9d5fd0
Kumar Sangam <40032010+Sangamdk@users.noreply.github.com>
2024-10-09 11:53:47 +0530

    Add files via upload

---
+++ javascript files/maltipication 1 table.js
@@ -0,0 +1,21 @@
+
+<html>
+<head>
+<script>
+// take number input from the user
+const number = parseInt(prompt('Enter an integer: '));
+
+// take range input from the user
+const range = parseInt(prompt('Enter a range: '));
+
+//creating a multiplication table
+for(let i = 1; i <= range; i++) {
+    const result = i * number;
+    console.log(`${number} * ${i} = ${result}`);
+}
+</script>
+</head>
+
+<body>
+</body>
+</html>
